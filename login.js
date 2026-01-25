3e667aef2ac9a67a04d16a357256f39e0738193e
Jacob Kalfa <yaacov.ofer@gmail.com>
2026-01-25 11:01:10 +0200

    עדכון אתר-25-1-26

--- login.js
+++ login.js
@@ -1,15 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+// בראש הקובץ login.js או admin-logic.js
+import { auth } from './firebase-config.js';
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCMs4kGS3lLScdyTZhD2ZEG-bjmb3nezCI",
-    authDomain: "kalfa-toaff-admin.firebaseapp.com",
-    projectId: "kalfa-toaff-admin",
-    storageBucket: "kalfa-toaff-admin.firebasestorage.app",
-    messagingSenderId: "1030816912499",
-    appId: "1:1030816912499:web:5de0933ee292793a67c606",
-    measurementId: "G-7PW96M5BHW"
-};
+// אין צורך יותר ב-firebaseConfig או ב-initializeApp כאן!
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
